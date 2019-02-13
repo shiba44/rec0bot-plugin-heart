08483b53dfa4f27955ed2108d9e5b4be826b521b
Arisaka Mashiro <me@clvs7.com>
2019-02-13 20:19:15 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Sindoi Reporter for REC0-Bot
+
+## What is this?
+A Rec0-bot plugin murmurs everyone's "しんどい (sindoi) ……".
+
+## How to build
+Run `npm i && npm run build` to build.  
+If you want to clean up built files, run `npm run clean`.
+
+## 環境変数
+- `REC0_ENV_SINDOI_CHANNEL` : A channel name which rec0bot posts murmur to.
+
+## Author
+Arisaka Mashiro
+
+## License
+Apache License 2.0
---
+++ index.ts
@@ -0,0 +1,32 @@
+import { Logger } from '@log4js-node/log4js-api';
+import * as path from 'path';
+import { BotProxy } from '../../src/interface/bot-proxy.interface';
+
+let mBot: BotProxy;
+let logger: Logger;
+let metadata: {[key: string]: string};
+
+export const init = async (bot: BotProxy, options: { [key: string]: any }): Promise<void> => {
+    mBot = bot;
+    logger = options.logger || console;
+    metadata = await import(path.resolve(__dirname, 'package.json'));
+
+    logger.info(`${metadata.name} plugin v${metadata.version} has been initialized.`);
+};
+
+export const onStart = () => {
+    logger.debug('onStart()');
+};
+
+export const onStop = () => {
+    logger.debug('onStop()');
+};
+
+export const onMessage = async (message: string, channelId: string, userId: string, data: { [key: string]: any }) => {
+    await mBot.sendTalk(await mBot.getChannelId(process.env.REC0_ENV_SINDOI_CHANNEL || 'sindoi'),
+        `しんどい……${message.split(' ').slice(1).join(' ')}`);
+};
+
+export const onPluginEvent = (eventName: string, value?: any, fromId?: string) => {
+    // Nop
+};
